7f116e6f23de8771b39ffcff060520f419056374
Mary Dang <dangmaryk@gmail.com>
2019-04-09 19:53:35 -0500

    fixed up a few more linking issues

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,9 +12,10 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(userData);
   });
+  
+  // API POST Requests
+  app.post("/api/friends", function(req,res) {
+    console.log(req.body);
+    //userData.push(req.body);
+  });
 };
-
-// API POST Requests
-app.post("/api/friends", function(req,res) {
-  userData.push(req.body);
-});
