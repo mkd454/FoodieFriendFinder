5f2a32ac83c6b4a162a8671bc728cc295d12aaa6
Mary Dang <dangmaryk@gmail.com>
2019-04-09 19:21:10 -0500

    updated friends data file & apiroutes

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+// DATA
+
+var usersArray = [
+  {
+    name: "Mary",
+    image: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/38009370_10160705761200607_8128982368032653312_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=823a52ab2d0f03d89a7479b908264c20&oe=5D43910D",
+    score: ["1",
+            "3",
+            "4",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "5",
+            "5"]
+  }
+];
+
+module.exports = usersArray;
